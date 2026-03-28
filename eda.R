3834cbbd6b06254feee293e07f2be31ac0cbb9c6
Sihan <sihan.zhuang@student.adelaide.edu.au>
2026-03-28 20:06:18 +1030

    Import raw data and inspect its structure

--- eda.R
+++ eda.R
@@ -1,1 +1,21 @@
 # Assignment 1 EDA
+# Exploratory Data Analysis (EDA)
+# Topic: Singapore Total Live Births (TLB) and Total Fertility Rate (TFR)
+
+# 1. Setup
+
+rm(list = ls())
+
+library(readr)
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+
+# 2. Import data
+
+raw_data <- read_csv("BirthsAndFertilityRatesAnnual.csv")
+
+glimpse(raw_data)
+head(raw_data)
+names(raw_data)
+
